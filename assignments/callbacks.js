14ada887ec93aca9584bb073f9cb19f660c8012d
Robert <Robert.Elias@live.com>
2019-10-15 13:04:04 -0700

    Callbacks completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,6 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works with our items array. 
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -39,27 +41,55 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 */
 
 
-function getLength(arr, cb) {
+const getLength = (arr, cb) => {
   // getLength passes the length of the array into the callback.
-}
+  cb(arr.length);
+};
+getLength(items, length => {
+  console.log(`The length of the array is ${length}.`);
+});
 
-function last(arr, cb) {
+const last = (arr, cb) => {
   // last passes the last item of the array into the callback.
-}
+  cb(arr[arr.length-1]);
+};
+last(items, lastItem => {
+  console.log(`The last item in the array is ${lastItem}`)
+});
 
-function sumNums(x, y, cb) {
+
+const sumNums = (x, y, cb) => {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+  cb(x + y);
+};
+sumNums(50, 20, sum =>{
+  console.log(`The sum of the x and y is ${sum}`)
+});
 
-function multiplyNums(x, y, cb) {
+const multiplyNums = (x, y, cb) => {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  cb(x * y);
+};
+multiplyNums(50 , 20, product => {
+  console.log(`The product of x and y is ${product}`)
+});
 
-function contains(item, list, cb) {
+
+const contains = (items, list, cb) => {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
-
+  const inside = () => {
+    for(let i = 0; i < items.length; i++){
+      if (items[i] === list) {
+        return true;
+      }
+    } return false;
+  };
+  cb(inside());
+};
+contains(items, 'Notebook', result =>{
+ console.log(result ? 'Notebook is in the array' : 'Notebook is not in the array');
+});
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
